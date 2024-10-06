2a7bb9bb00be3c52a98217f851d0f0dcf37c72d4
AndyMcGamer <andymcgamer@gmail.com>
2024-10-06 16:55:28 -0700

    added option to load additive scenes as well as unload scenes

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -21,25 +21,53 @@ public class SceneLoader : MonoBehaviour
     /// Loads the specified scene.
     /// </summary>
     /// <param name="sceneName"></param>
-    public void LoadScene(string sceneName)
+    public void LoadScene(string sceneName, UnityEngine.SceneManagement.LoadSceneMode sceneMode = UnityEngine.SceneManagement.LoadSceneMode.Single)
     {
-        StartCoroutine(LoadSceneAsync(sceneName));
+        StartCoroutine(LoadSceneAsync(sceneName, sceneMode));
     }
 
     /// <summary>
     /// Loads the specified scene.
     /// </summary>
     /// <param name="index"></param>
-    public void LoadScene(int index)
+    public void LoadScene(int index, UnityEngine.SceneManagement.LoadSceneMode sceneMode = UnityEngine.SceneManagement.LoadSceneMode.Single)
     {
         var sceneName = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(index));
-        StartCoroutine(LoadSceneAsync(sceneName));
+        StartCoroutine(LoadSceneAsync(sceneName, sceneMode));
     }
 
-    private IEnumerator LoadSceneAsync(string sceneName)
+    private IEnumerator LoadSceneAsync(string sceneName, UnityEngine.SceneManagement.LoadSceneMode sceneMode)
     {
-        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName, sceneMode);
         while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Unloads the specified scene.
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void UnloadScene(string sceneName)
+    {
+        StartCoroutine(UnloadSceneAsync(sceneName));
+    }
+
+    /// <summary>
+    /// Unloads the specified scene.
+    /// </summary>
+    /// <param name="index"></param>
+    public void UnloadScene(int index)
+    {
+        var sceneName = System.IO.Path.GetFileNameWithoutExtension(UnityEngine.SceneManagement.SceneUtility.GetScenePathByBuildIndex(index));
+        StartCoroutine(UnloadSceneAsync(sceneName));
+    }
+
+    private IEnumerator UnloadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncUnload = UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneName);
+        while (!asyncUnload.isDone)
         {
             yield return null;
         }
